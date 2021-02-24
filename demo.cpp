5e6ba194e1f442f8f0436a10b80edc5a98eefc0a
Adrian Ravai <ravaiadrian@gmail.com>
2021-02-24 12:42:40 +0100

    Removed unnecessary output formatting.

--- demo.cpp
+++ demo.cpp
@@ -7,21 +7,12 @@ int main() {
 
   std::list<int> randoms;
   const int numberOfRandoms = 1000;
-  const int lineLength = 100;
 
   for (int i = 0; i < numberOfRandoms; i++)
-    randoms.emplace_back(LCG::Rand(-10, 10));
-
-  int counter = 0;
+    randoms.emplace_back(LCG::Rand(-1000, 1000));
 
   std::for_each(randoms.begin(), randoms.end(), [&](const int random) {
       std::cout << random << " ";
-      counter++;
-
-      if (counter == lineLength) {
-        std::cout << std::endl;
-        counter = 0;
-      }
   });
 
   return 0;
